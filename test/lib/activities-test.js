a044a1bf6f6d6108388b1a5e87998411e4cdb03a
Dan McAulay <dmcaulay@gmail.com>
2013-04-09 22:57:13 -0700

    initial commit

---
+++ test/lib/activities-test.js
@@ -0,0 +1,18 @@
+var assert = require('assert')
+var config = require('config')
+var avocado = require('avocado-api')
+avocado.setup(config)
+var session = avocado.login(config.credentials)
+var activities = require('../../lib/activities')
+
+describe('avocado activities', function() {
+  describe('mostRecent', function() {
+    it('returns the most recent activites', function(done) {
+      activities.mostRecent(session, function(err, res) {
+        assert.ifError(err)
+        done()
+      })
+    })
+  })
+})
+
